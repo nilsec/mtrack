e2d496bc819b166f5d81087ab5624cba1b14107d
Nils <nils.eckstein@googlemail.com>
2017-06-09 13:06:36 +0200

    Add function to transform g2 solution to g1 solution

--- solve.py
+++ solve.py
@@ -10,7 +10,7 @@ def solve(g1,
           comb_angle_factor,
           selection_cost,
           output_directory,
-          time_limit)
+          time_limit):
 
     vertex_cost_params = {}
     edge_cost_params = {"distance_factor": distance_factor,
@@ -23,7 +23,7 @@ def solve(g1,
         g1_tmp = g1_graph.G1(0) # initialize empty G1 graph
         g1 = g1_tmp.load(g1) # load from file
 
-    if self.g1.get_number_of_edges() == 0:
+    if g1.get_number_of_edges() == 0:
         raise Warning("Graph has no edges.")
 
     print "Get G2 graph..."
@@ -40,12 +40,57 @@ def solve(g1,
 
     print "Set G2 costs..."
     for v in g2.get_vertex_iterator():
-        self.g2.set_cost(v, g2_cost[v])
+        g2.set_cost(v, g2_cost[v])
 
     print "Create ILP..."
     solver = g2_solver.G2Solver(g2)
     
     print "Solve ILP..."
-    g2_solution = solver.solve(time_limit=time_limit, verbose=True)
+    g2_solution = solver.solve(time_limit=time_limit)
 
-    return g2_solution    
+    print "Get G1 solution..."
+    g1_solution = g2_to_g1_solution(g2_solution, g1, g2, index_maps)
+
+    return g1_solution
+
+
+def g2_to_g1_solution(g2_solution, g1, g2, index_maps):
+    g1_selected_edges = set()
+    g1_selected_vertices = set()
+
+    for v in g2.get_vertex_iterator():
+        
+        if g2_solution[v] > 0.5:
+            g1e_in_g2v = index_maps["g2vertex_g1edges"][v]
+            
+            for e in g1e_in_g2v:
+                if e.source() != g1_graph.G1.START_EDGE.source():
+                    g1_selected_edges.add(e)
+                    g1_selected_vertices.add(e.source())
+                    g1_selected_vertices.add(e.target())
+
+    edge_mask = []
+    vertex_mask = []
+
+    for v in g1.get_vertex_iterator():
+        if v in g1_selected_vertices:
+            vertex_mask.append(True)
+        else:
+            vertex_mask.append(False)
+
+    for e in g1.get_edge_iterator():
+        if e in g1_selected_edges:
+            edge_mask.append(True)
+        else:
+            edge_mask.append(False)
+
+    vertex_filter = g1.g.new_vertex_property("bool")
+    edge_filter = g1.g.new_edge_property("bool")
+
+    vertex_filter.a = vertex_mask
+    edge_filter.a = edge_mask
+
+    g1.g.set_vertex_filter(vertex_filter)
+    g1.g.set_edge_filter(edge_filter)
+
+    return g1
