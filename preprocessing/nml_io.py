3fe71893858c88cae1ee14d55c40dcdb4507c909
Nils <nils.eckstein@googlemail.com>
2017-06-20 10:57:49 +0200

    Add unfction to load attributes from nml file

--- preprocessing/nml_io.py
+++ preprocessing/nml_io.py
@@ -2,6 +2,7 @@ import numpy as np
 from xml.dom import minidom
 import os
 import sys
+import ast
 
 class NodeAttribute:
     def __init__(self, position, id, orientation, partner, identifier):
@@ -124,23 +125,67 @@ def build_attributes(xml_elem, attributes):
     return xml_elem
 
 
-def nml_to_g1():
-    return 0
-
-
 def from_nml(filename):
     doc = minidom.parse(filename)
     annotation_elems = doc.getElementsByTagName("thing")
-    
     node_dic = {}
     edge_list = []
-
     for annotation_elem in annotation_elems:
         node_elems = annotation_elem.getElementsByTagName("node")
-        id_elems = annotation_elem.getElementsByTagName("thing")
-        print id_elems
-        for id_elem in id_elems:
-            print parse_attributes([["id", int]])
+        for node_elem in node_elems:
+            #point, id, orientation, partner, identifier = from_node_elem_to_node(node_elem)
+            node_attribute = NodeAttribute(*from_node_elem_to_node(node_elem))
+            if node_attribute.id in node_dic:
+                print 'WARNING: ID already exists'
+                break
+            else:
+                #point_dic[id] = [point, id, orientation, partner, identifier]
+                node_dic[node_attribute.id] = node_attribute
+
+        edge_elems = annotation_elem.getElementsByTagName("edge")
+        for edge_elem in edge_elems:
+            (source_ID, target_ID) = parse_attributes(edge_elem, [["source", int], ["target", int]])
+            edge_list.append([source_ID, target_ID])
+
+    return node_dic, edge_list
+
+def from_node_elem_to_node(node_elem):
+    [x, y, z, ID, radius, orientation, partner, identifier] =\
+        parse_attributes(node_elem, 
+                        [
+                            ["x", float], 
+                            ["y", float], 
+                            ["z", float], 
+                            ["id", int], 
+                            ["radius", float], 
+                            ["orientation", str], 
+                            ["partner", int], 
+                            ["identifier", int]
+                        ]
+                        )
+
+    point = np.array([x, y, z])
+    
+    if orientation is not None:
+        orientation = orientation.replace("[", " ").replace("]", " ")
+        split = orientation.split(" ")
+        rec_ori = []
+        for i in split:
+            try:
+                rec_ori.append(float(i))
+            except:
+                continue
+
+        orientation = np.array(rec_ori)
+        for j in xrange(np.shape(orientation)[0]):
+            orientation[j] = float(orientation[j])
+    else:
+        orientation = np.array([0., 0., 0.])
+
+    return point, ID, np.array(orientation), partner, identifier
+
+
+
 
 def parse_attributes(xml_elem, parse_input):
     parse_output = []
