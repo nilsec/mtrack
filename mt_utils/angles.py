d8787764780a49427fdbc822617b009c3f9ba2d7
Nils <nils.eckstein@googlemail.com>
2017-06-27 18:56:34 +0200

    Optimize speed

--- mt_utils/angles.py
+++ mt_utils/angles.py
@@ -1,36 +1,28 @@
 import numpy as np
+from numpy.linalg import norm
 import math
 
 def get_spanning_angle(vec_1, vec_2):
-    v1 = np.array(vec_1)
-    v2 = np.array(vec_2)
-    assert len(v1) == 3
-    assert len(v2) == 3
+    assert vec_1.shape[0] == 3 and vec_2.shape[0] == 3
 
-    if sum(v1) == 0 or sum(v2) == 0:
-        return 0
-    elif (v1 == v2).all():
-        return 0
-    elif ((v1 * (-1)) == v2).all():
-        # print "zero or 180 degree"
-        return np.pi
-    else:
-        try:
-            angle = math.acos(np.dot(v1, v2) / ((np.linalg.norm(v1)) * np.linalg.norm(v2)))
-            return angle
-        except ValueError:
-            print "value close to zero"
-            #     print v1, v2
-            angle = 0
-    return angle
+    norm_1 = math.sqrt(vec_1[0]**2 + vec_1[1]**2 + vec_1[2]**2) 
+    norm_2 = math.sqrt(vec_2[0]**2 + vec_2[1]**2 + vec_2[2]**2)
 
+    u1 = vec_1/norm_1
+    u2 = vec_2/norm_2
+
+    angle = np.arccos(np.clip(np.dot(u1, u2), -1.0, 1.0))
+    return angle 
+ 
+    
 def get_orientation_angle(v_positions, v_orientations):
     assert(len(v_positions) == 2)
     assert(len(v_orientations) == 2)
 
     vector = v_positions[0] - v_positions[1]
-    orientation_angle = []
+    orientation_angle = np.empty(2)
 
+    j = 0
     for orientation in v_orientations:
         angle = get_spanning_angle(vector, orientation)
         
@@ -38,6 +30,7 @@ def get_orientation_angle(v_positions, v_orientations):
             assert(angle <= np.pi)
             angle = np.pi - angle
 
-        orientation_angle.append(angle)
+        orientation_angle[j] = angle
+        j += 1
 
     return orientation_angle
