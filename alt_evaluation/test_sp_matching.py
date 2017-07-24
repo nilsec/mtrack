0c8ecfc9903bfba892ca4f5d0b85051dfb2b0124
Nils <nils.eckstein@googlemail.com>
2017-07-24 14:35:31 +0200

    Add test for error graph class

--- alt_evaluation/test_sp_matching.py
+++ alt_evaluation/test_sp_matching.py
@@ -55,7 +55,63 @@ class GetRecChainsTestCase(SimpleLines):
                                                 voxel_size=[5.,5.,50.])
 
         for chain in rec_chains:
+            chain.get_shortest_path(plot=False)
+
+class MergeTestCase(unittest.TestCase):
+    def setUp(self):
+        N = 10
+        gt_line_1 = graphs.g1_graph.G1(N)
+        gt_line_2 = graphs.g1_graph.G1(N)
+
+        rec_line_1 = graphs.g1_graph.G1(N)
+
+        #Make line:
+        for v in range(N - 1):
+            gt_line_1.add_edge(v, v+1)
+            gt_line_2.add_edge(v, v+1)
+
+            rec_line_1.add_edge(v, v+1)
+
+        z = 2.0
+        for v in gt_line_1.get_vertex_iterator():
+            gt_line_1.set_position(v, np.array([3.0, 3.0, z]))
+            gt_line_2.set_position(v, np.array([9.0, 9.0, z]))
+            z += 1
+
+        pos_rec =  [np.array([4.0, 4.0, 2.0]),
+                    np.array([4.0, 4.0, 3.0]),
+                    np.array([4.0, 4.0, 4.0]),
+                    np.array([5.0, 5.0, 5.0]), 
+                    np.array([6.0, 6.0, 6.0]), 
+                    np.array([7.0, 7.0, 7.0]),
+                    np.array([8.0, 8.0, 8.0]),
+                    np.array([8.0, 8.0, 9.0]),
+                    np.array([8.0, 8.0, 10.0]),
+                    np.array([8.0, 8.0, 11.0])]
+
+        for v, pos in zip(rec_line_1.get_vertex_iterator(), pos_rec):
+            rec_line_1.set_position(v, pos)
+
+        self.gt_lines = [gt_line_1, gt_line_2]
+        self.rec_lines = [rec_line_1]
+           
+ 
+
+class ErrorGraphTestCase(MergeTestCase):
+    def runTest(self):
+        rec_chains = sp_matching.get_rec_chains(self.rec_lines,
+                                                self.gt_lines,
+                                                dimensions=[12, 12, 12],
+                                                tolerance_radius=30.0,
+                                                voxel_size=[5.,5.,50.])
+
+        for chain in rec_chains:
             chain.get_shortest_path(plot=True)
+
+        error_graph = sp_matching.ErrorGraph(rec_chains)
+        error_graph.get_sp_matching(3)
+        error_graph.draw()
+ 
 
 
         
