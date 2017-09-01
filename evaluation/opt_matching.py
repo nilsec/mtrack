6be5a26718c82529c281cc6fa4c8c61ea5da0c7f
Nils <nils.eckstein@googlemail.com>
2017-09-01 16:57:11 +0200

    Add constraint structure.

--- evaluation/opt_matching.py
+++ evaluation/opt_matching.py
@@ -7,7 +7,8 @@ def is_arr_in_list(arr, arr_list):
     return next((True for elem in arr_list if elem is arr), False)
 
 class OptMatch(object):
-    def __init__(self, lines_gt, lines_rec, n, distance_tolerance, voxel_size=np.array([5.0,5.0,50.0])):
+    def __init__(self, lines_gt, lines_rec, n, distance_tolerance, dummy_cost, 
+                 pair_cost_factor, max_edges, voxel_size=np.array([5.0,5.0,50.0])):
         assert(isinstance(lines_gt, list))
         assert(isinstance(lines_rec, list))
 
@@ -43,15 +44,279 @@ class OptMatch(object):
                                               self.inv_gt_chunk_positions[tuple(pos_0)]))
 
         self.edge_pairs, self.edge_pair_cost = self.get_edge_pairs(self.gt_rec_pairs)
-        pdb.set_trace()
         
         self.backend = pylp.GurobiBackend()
-        self.backend.initialize(len(self.rec_chunks.values() + self.gt_chunks.values()), 
-                                    pylp.VariableType.Binary)
-        self.backend.initialize(len(self.gt_rec_pairs), pylp.VariableType.Binary)
-        self.backend.initialize(len(self.edge_pairs), pylp.VariableType.Binary)    
+
+        self.n_rec_nodes = len(self.rec_chunks.values())
+        self.n_gt_nodes = len(self.gt_chunks.values())
+        self.n_gt_rec_edges = len(self.gt_rec_pairs)
+        self.n_edge_pairs = len(self.edge_pairs)
+        self.n_dummy_edges = self.n_rec_nodes + self.n_gt_nodes
+
+        self.n_tot = self.n_rec_nodes + self.n_gt_nodes +\
+                     self.n_gt_rec_edges + self.n_edge_pairs +\
+                     self.n_dummy_edges
+
+        self.backend.initialize(self.n_tot, 
+                                pylp.VariableType.Binary)
+
+        # Costs
+        self.objective = pylp.LinearObjective(self.n_tot)
+        
+        start_id = 0
+        self.rec_node_start_id = start_id
+        for rec_node_id in range(start_id, self.n_rec_nodes):
+            self.objective.set_coefficient(rec_node_id,
+                                           0.0)
+
+        start_id += self.n_rec_nodes
+        self.gt_node_start_id = start_id
+        for gt_node_id in range(start_id, start_id + self.n_gt_nodes):
+            self.objective.set_coefficient(gt_node_id,
+                                           0.0)
+
+        start_id += self.n_gt_nodes
+        self.edge_start_id = start_id
+        for edge_id in range(start_id, start_id + self.n_gt_rec_edges):
+            self.objective.set_coefficient(edge_id,
+                                           0.0)
+
+        start_id += self.n_gt_rec_edges
+        self.edge_pair_start_id = start_id
+        for edge_pair_id in range(start_id, start_id + self.n_edge_pairs):
+            self.objective.set_coefficient(edge_pair_id,
+                                           self.edge_pair_cost[edge_pair_id - start_id] * pair_cost_factor)
+
+        start_id += self.n_edge_pairs
+        self.dummy_start_id = start_id
+        for dummy_edge_id in range(start_id, start_id + self.n_dummy_edges):
+            self.objective.set_coefficient(dummy_edge_id,
+                                           dummy_cost)
+
+        self.backend.set_objective(self.objective)
+
+        # Conflicts & Constraints
+        self.constraints = pylp.LinearConstraints()
+
+        edge_pair_constraints = self.get_edge_pair_constraints()
+        edge_constraints = self.get_edge_constraints()
+        node_edges = self.get_edges_to_node()
+        dummy_constraints = self.get_dummy_constraints()
+        cross_conflicts = self.get_cross_conflicts()
+        id_conflicts = self.get_id_conflicts()
+
+        for edge_pair in edge_pair_constraints:
+            # Pair implies edges
+            constraint = pylp.LinearConstraint()
+
+            constraint.set_coefficient(edge_pair[0], 2) # pair
+            constraint.set_coefficient(edge_pair[1], -1)
+            constraint.set_coefficient(edge_pair[2], -1)
+            constraint.set_relation(pylp.Relation.LessEqual)
+            constraint.set_value(0)
+
+            self.constraints.add(constraint)
+
+            # Edges imply pair
+            constraint = pylp.LinearConstraint()
+
+            constraint.set_coefficient(edge_pair[1], 1)
+            constraint.set_coefficient(edge_pair[2], 1)
+            constraint.set_coefficient(edge_pair[0], -1) # pair
+            constraint.set_relation(pylp.Relation.LessEqual)
+            constraint.set_value(1)
+            
+            self.constraints.add(constraint)
+
+        for edge_nodes in edge_constraints:
+            # Edge implies nodes
+            constraint = pylp.LinearConstraint()
+            
+            constraint.set_coefficient(edge_nodes[0], 2)
+            constraint.set_coefficient(edge_nodes[1], -1)
+            constraint.set_coefficient(edge_nodes[2], -1)
+            constraint.set_relation(pylp.Relation.LessEqual)
+            constraint.set_value(0)
     
-    
+            self.constraints.add(constraint)
+
+        for edges_to_node in node_edges:
+            # node_edges = [(node_id, edge_to_node_1, edge_to_node_2, ...), ...]
+
+            # If the dummy node is chosen no other edge can be chosen
+            constraint = pylp.LinearConstraint()
+
+            for edge in edges_to_node[1:]:
+                constraint.set_coefficient(edge, 1)
+
+            #dummy node corresponding to node_i:
+            dummy = edges_to_node[0] + self.dummy_start_id
+            constraint.set_coefficient(dummy, max_edges)
+            constraint.set_relation(pylp.Relation.LessEqual)
+            constraint.set_value(max_edges)
+            
+            self.constraints.add(constraint)
+
+            # Every node needs to be explained by dummy or edge
+            constraint = pylp.LinearConstraint()
+
+            for edge in edges_to_node[1:]:
+                constraint.set_coefficient(edge, 1)
+            
+            constraint.set_coefficient(dummy, max_edges)
+            constraint.set_relation(pylp.Relation.GreaterEqual)
+            constraint.set_value(1)
+
+            self.constraints.add(constraint)
+
+        for dummy_node in dummy_constraints:
+            # Dummy implies node:
+            constraint = pylp.LinearConstraint()
+        
+            constraint.set_coefficient(dummy_node[0], 1)
+            constraint.set_coefficient(dummy_node[1], -1)
+            constraint.set_relation(pylp.Relation.LessEqual)
+            constraint.set_value(0)
+
+            self.constraints.add(constraint)
+
+        for cross_edges in cross_conflicts:
+            # Crossing edges are exclusive
+            assert(len(cross_edges) == 2)
+            constraint = pylp.LinearConstraint()
+            
+            for edge in cross_edges:
+                constraint.set_coefficient(edge, 1)
+
+            constraint.set_relation(pylp.Relation.LessEqual)
+            constraint.set_value(1)
+            
+            self.constraints.add(constraint)
+
+        for id_conflict in id_conflicts:
+            # Each node can only have edges that point to same line id
+            assert(len(id_conflict) == 2)
+            constraint = pylp.LinearConstraint()
+        
+            for edge in id_conflict:
+                pdb.set_trace()
+                constraint.set_coefficient(edge, 1)
+
+            constraint.set_relation(pylp.Relation.LessEqual)
+            constraint.set_value(1)
+
+            self.constraints.add(constraint)
+        
+        print "Yay"
+        pdb.set_trace()
+
+    def get_edge_pair_constraints(self):
+        constraints = [] # constraints will be tuple of the form (edge_pair_id, edge_id_1, edge_id_2)
+        
+        edge_pair_id = 0
+        for edge_pair in self.edge_pairs:
+            edge_id_1 = self.gt_rec_pairs.index(edge_pair[0])
+            edge_id_2 = self.gt_rec_pairs.index(edge_pair[1])
+            constraints.append((edge_pair_id + self.edge_pair_start_id, 
+                                edge_id_1 + self.edge_start_id, 
+                                edge_id_2 + self.edge_start_id))
+            edge_pair_id += 1
+
+        return constraints
+
+    def get_edge_constraints(self):
+        constraints = [] # constraints will be tuple of the form (edge_id, node_id_1, node_id_2)
+        node_list = self.rec_chunks.keys() + self.gt_chunks.keys()
+
+        edge_id = 0
+        for edge in self.gt_rec_pairs:
+            node_id_1 = node_list.index(edge[0])
+            node_id_2 = node_list.index(edge[1])
+            constraints.append((edge_id + self.edge_start_id, 
+                                node_id_1 + self.rec_node_start_id, 
+                                node_id_2 + self.rec_node_start_id))
+            edge_id += 1
+
+        return constraints
+
+    def get_dummy_constraints(self):
+        # dummy - node <= 0 i.e. dummy implies node
+        # constraints = [(dummy, node), ...]
+        node_list = self.rec_chunks.keys() + self.gt_chunks.keys()
+
+        constraints = [(i + self.dummy_start_id, i + self.rec_node_start_id) for i in range(len(node_list))]
+        
+        return constraints
+
+    def get_cross_conflicts(self):
+        # We have exclusive edges that cross:
+        # An edge is in conflict with all other edges for which:
+        # e_1[0][1] > e_2[0][1] and e_1[1][1] < e_2[1][1]
+
+        conflicts = []
+        for i in range(len(self.gt_rec_pairs)):
+            edge_i = self.gt_rec_pairs[i]
+            c_i = set([i + self.edge_start_id])
+
+            for j in range(len(self.gt_rec_pairs)):
+
+                edge_j = self.gt_rec_pairs[j]
+
+                if edge_i[0][1] > edge_j[0][1]:
+                    if edge_i[1][1] < edge_j[1][1]:
+                        c_i.add(j + self.edge_start_id)
+                        conflicts.append(tuple(c_i))
+
+                        c_i = set([i + self.edge_start_id])
+                        
+        return conflicts
+                    
+    def get_edges_to_node(self):
+        node_list = self.rec_chunks.keys() + self.gt_chunks.keys()
+        
+        node_edges = [] # [(node_id, edge_to_node_1, edge_to_node_2, ...), ...]        
+        node_id = 0
+        for node in node_list:
+
+            edges_to_node = []
+            edge_id = 0
+            for edge in self.gt_rec_pairs:
+                if node in edge:
+                    edges_to_node.append(edge_id + self.edge_start_id)
+
+                edge_id += 1
+
+            tmp = [node_id + self.rec_node_start_id] + edges_to_node
+            node_edges.append(tuple(tmp))
+
+            node_id += 1
+
+        return node_edges
+
+    def get_id_conflicts(self):
+        conflicts = []
+        edges_to_node = self.get_edges_to_node()
+        node_list = self.rec_chunks.keys() + self.gt_chunks.keys()
+
+        for node_edges in edges_to_node:
+            for i in range(1, len(node_edges)):
+                for j in range(i + 1, len(node_edges)):
+                   
+                    node = node_list[node_edges[0] - self.rec_node_start_id] 
+                    edge_i = self.gt_rec_pairs[node_edges[i] - self.edge_start_id]
+                    edge_j = self.gt_rec_pairs[node_edges[j] - self.edge_start_id]
+
+                    node_id_i = edge_i.index(node)
+                    node_id_j = edge_j.index(node)
+
+                    pdb.set_trace()
+
+                    if edge_i[int(not node_id_i)][0] != edge_j[int(not node_id_j)][0]:
+                        conflicts.append((edges_to_node[i], edges_to_node[j]))
+
+        pdb.set_trace()
+        return conflicts
+
     def get_chunks(self, lines, n, l_0=0):
         chunks = {}
         chunk_positions = {}
