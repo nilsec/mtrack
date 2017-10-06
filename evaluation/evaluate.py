0c7be52673874ebdc3705fc6ed8d07f3e8416ba8
Nils <nils.eckstein@googlemail.com>
2017-10-06 19:25:54 +0200

    Update parameters

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -4,6 +4,7 @@ from process_solution import get_lines
 from graphs import G1
 import os
 import pdb
+import pylp
 
 def evaluate(tracing_file, 
              solution_file,
@@ -13,19 +14,24 @@ def evaluate(tracing_file,
              edge_selection_cost,
              pair_cost_factor,
              max_edges,
-             voxel_size=[5.,5.,50.]):
+             voxel_size=[5.,5.,50.],
+             tracing_line_paths=None,
+             rec_line_paths=None):
 
     line_base_dir = None
-    if cc_solution_dir[-1] == "/":
-        line_base_dir = os.path.dirname(cc_solution_dir[:-1]) + "/lines"
+    if solution_file[-1] == "/":
+        line_base_dir = os.path.dirname(solution_file[:-1]) + "/lines"
     else:
-        line_base_dir = os.path.dirname(cc_solution_dir) + "/lines"
-        
-    tracing_line_paths = get_lines(tracing_file, 
+        line_base_dir = os.path.dirname(solution_file) + "/lines"
+
+    
+    if tracing_line_paths is None:    
+        tracing_line_paths = get_lines(tracing_file, 
                               line_base_dir + "/tracing/", 
                               nml=True)
 
-    rec_line_paths = get_lines(solution_file, 
+    if rec_line_paths is None:
+        rec_line_paths = get_lines(solution_file, 
                           line_base_dir + "/reconstruction/",
                           nml=True)
 
@@ -36,21 +42,36 @@ def evaluate(tracing_file,
                        chunk_size,
                        distance_tolerance,
                        dummy_cost,
-                       edge_selection_cost,
                        pair_cost_factor,
-                       max_edges)
+                       max_edges,
+                       edge_selection_cost)
+    
+    print "LOGLEVEL:", pylp.getLogLevel()
 
-    matcher.solve()
+
+    solution = matcher.solve()
+    matcher.evaluate_solution(solution, tracing_line_paths, rec_line_paths, 
+                                        os.path.dirname(line_base_dir) + "/evaluation")
     
 if __name__ == "__main__":
     tracing_file = "/media/nilsec/d0/gt_mt_data/test_tracing/v18_cropped_small_300_309.nml"
-    cc_solution_dir = "/media/nilsec/d0/gt_mt_data/solve_volumes/test_volume_300_309/solution/volume.gt"
+    solution_file_0404 = "/media/nilsec/d0/gt_mt_data/solve_volumes/test_volume_300_309/solution/volume.gt"
+    solution_file_0304 = "/media/nilsec/d0/gt_mt_data/solve_volumes/test_volume_ps0304_300_309/solution/volume.gt"
+
+    solution_file_0303 = "/media/nilsec/d0/gt_mt_data/solve_volumes/test_volume_ps0303_300_309/solution/volume.gt"
+ 
+
+    tracing_line_paths = ["/media/nilsec/d0/gt_mt_data/solve_volumes/test_volume_300_309/solution/minimal_lines/tracing/cc33_min1_phy.gt"]
+
+    rec_line_paths = ["/media/nilsec/d0/gt_mt_data/solve_volumes/test_volume_300_309/solution/minimal_lines/reconstruction/cc6_min1_phy.gt"]
 
     evaluate(tracing_file, 
-             cc_solution_dir,
-             chunk_size=3,
+             solution_file_0303,
+             chunk_size=10,
              distance_tolerance=100.0,
-             dummy_cost=100.0,
-             edge_selection_cost=0.0,
+             dummy_cost=100000,
+             edge_selection_cost=-10.0,
              pair_cost_factor=1.0,
-             max_edges=3)
+             max_edges=5,
+             tracing_line_paths=None,
+             rec_line_paths=None)
