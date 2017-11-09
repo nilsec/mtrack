23d5dccaaa4345d993c7ecfd31b98d6a840bda26
Nils <nils.eckstein@googlemail.com>
2017-11-09 02:50:41 +0100

    Update init

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,3 +1,4 @@
 from nml_io import g1_to_nml, nml_to_g1
 from extract_canidates import extract_candidates, DirectionType, candidates_to_g1, connect_graph_locally
 from lemon_io import g1_to_lemon
+from chunker import Chunker
