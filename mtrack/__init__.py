4a2c5136e3e22519e8f6165caaae80f9727ecc48
Nils <nils.eckstein@googlemail.com>
2017-11-21 06:02:23 +0100

    Update init for package structure

---
+++ mtrack/__init__.py
@@ -0,0 +1,1 @@
+from mtrack import *
--- mtrack/evaluation/__init__.py
+++ mtrack/evaluation/__init__.py
@@ -1,5 +1,6 @@
-from process_solution import get_lines
-from opt_matching import OptMatch, interpolate_nodes
-from dda3 import DDA3
-from evaluation_parser import EvaluationParser
-from evaluate import evaluate
+from .process_solution import get_lines
+from .opt_matching import OptMatch, interpolate_nodes
+from .dda3 import DDA3
+from .evaluation_parser import EvaluationParser
+from .evaluate import evaluate
+from .stitch_chunks_greedy import Stitcher
