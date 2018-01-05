f1c253eaa7da622970130e7550208406b6bd74f2
Nils <nils.eckstein@googlemail.com>
2018-01-05 17:44:02 +0100

    Finish track

--- mtrack/track.py
+++ mtrack/track.py
@@ -7,13 +7,13 @@ import h5py
 import pdb
 import os
 
-def chunk(volume_shape,
-          max_chunk_shape,
-          voxel_size,
-          overlap,
-          prob_map_perp_dir,
-          prob_map_par_dir,
-          output_dir):
+def chunk_pms(volume_shape,
+              max_chunk_shape,
+              voxel_size,
+              overlap,
+              prob_map_perp_dir,
+              prob_map_par_dir,
+              output_dir):
 
     dir_perp = os.path.join(output_dir, "perp")
     dir_par = os.path.join(output_dir, "par") 
@@ -92,12 +92,30 @@ def extract_pm_chunks(pm_chunks_par,
 
 def solve_candidate_volume(name_db,
                            collection,
+                           distance_threshold,
+                           cc_min_vertices,
+                           start_edge_prior,
+                           selection_cost,
+                           distance_factor,
+                           orientation_factor,
+                           comb_angle_factor,
+                           time_limit,
+                           hcs,
                            core_size,
                            context_size,
                            volume_size,
                            min_core_overlap,
+                           voxel_size,
+                           output_dir,
+                           save_candidates,
+                           save_cores,
+                           gt,
+                           nml,
+                           x_lim_out,
+                           y_lim_out,
+                           z_lim_out,
                            copy_candidates=True,
-                           copy_target="microtubules"
+                           copy_target="microtubules",
                            offset=np.array([0.,0.,0.])):
 
     solver = CoreSolver()
@@ -109,7 +127,7 @@ def solve_candidate_volume(name_db,
                     {"$out": copy_target},]
 
         db = solver._get_db(name_db)
-        db.source_collection.aggregate(pipeline)
+        db["candidates"].aggregate(pipeline)
         collection = copy_target
     
         
@@ -129,26 +147,35 @@ def solve_candidate_volume(name_db,
                                               x_lim=core.x_lim_context,
                                               y_lim=core.y_lim_context,
                                               z_lim=core.z_lim_context)
+        
+        if save_candidates:
+            g1, index_map = solver.subgraph_to_g1(vertices, 
+                                                  edges)
+            if gt:
+                candidates_core_dir = os.path.join(output_dir, "cores/candidates")
 
-        g1, index_map = solver.subgraph_to_g1(vertices, 
-                                              edges)
-        g1_to_nml(g1, 
-                  "./candidates2_core_{}.nml".format(n), 
-                  knossos=True, 
-                  voxel_size=[5.,5.,50.])
+                if not os.path.exists(candidates_core_dir):
+                    os.makedirs(candidates_core_dir)
+            
+                g1.save(os.path.join(candidates_core_dir, 
+                                     "core_{}.gt".format(n)))
+            if nml:
+                g1_to_nml(g1, 
+                          os.path.join(candidates_core_dir, "core_{}.nml".format(n)), 
+                          knossos=True, 
+                          voxel_size=voxel_size)
  
-    
         solutions = solver.solve_subgraph(g1,
                                           index_map,
-                                          distance_threshold=175,
-                                          cc_min_vertices=4,
-                                          start_edge_prior=160.0,
-                                          selection_cost=-70.0,
-                                          distance_factor=0.0,
-                                          orientation_factor=15.0,
-                                          comb_angle_factor=16.0,
-                                          time_limit=300,
-                                          hcs=False)
+                                          distance_threshold=distance_threshold,
+                                          cc_min_vertices=cc_min_vertices,
+                                          start_edge_prior=start_edge_prior,
+                                          selection_cost=selection_cost,
+                                          distance_factor=distance_factor,
+                                          orientation_factor=orientation_factor,
+                                          comb_angle_factor=comb_angle_factor,
+                                          time_limit=time_limit,
+                                          hcs=hcs)
 
         for solution in solutions:
             solver.write_solution(solution, 
@@ -165,15 +192,34 @@ def solve_candidate_volume(name_db,
                                      y_lim=core.y_lim_core,
                                      z_lim=core.z_lim_core)
 
-        vertices, edges = solver.get_subgraph(name_db,
-                                              collection,
-                                              x_lim={"min": offset[0], "max": 1024 * 5 + offset[0]},
-                                              y_lim={"min": offset[1], "max": 1024 * 5 + offset[1]},
-                                              z_lim={"min": offset[2], "max": 120 * 50 + offset[2]})
+        if save_cores:
+            vertices, edges = solver.get_subgraph(name_db,
+                                                  collection,
+                                                  x_lim=x_lim_out,
+                                                  y_lim=y_lim_out,
+                                                  z_lim=z_lim_out)
 
-        g1, index_map = solver.subgraph_to_g1(vertices, edges, set_partner=False)
-    
-        g1_to_nml(g1, "./solution3_core{}.nml".format(n), knossos=True, voxel_size=[5.,5.,50.])
+            g1, index_map = solver.subgraph_to_g1(vertices, edges, set_partner=False)
+
+            if gt:
+                gt_core_dir = os.path.join(os.path.join(output_dir, "cores/gt"))
+
+                if not os.path.exists(gt_core_dir):
+                    os.makedirs(gt_core_dir)
+
+                g1.save(os.path.join(gt_core_dir, "core_{}.gt".format(n)))
+
+            if nml:
+                nml_core_dir = os.path.join(os.path.join(output_dir, "cores/nml"))
+                
+                if not os.path.exists(nml_core_dir):
+                    os.makedirs(nml_core_dir)
+
+                g1_to_nml(g1, 
+                          os.path.join(nml_core_dir, "core{}.nml".format(n)), 
+                          knossos=True, 
+                          voxel_size=voxel_size)
+
         n += 1
 
 
@@ -200,6 +246,18 @@ def clean_up(name_db, collection, x_lim, y_lim, z_lim):
 def track(config_path):
     config = read_config(config_path)
     roi = [config["roi_x"], config["roi_y"], config["roi_z"]]
+
+
+    """
+    If all rois are [0,-1] extract candidates from
+    all chunks, i.e. all candidates.
+    """
+    full_roi = np.array([False, False, False])
+    for i in range(3):
+        if list(roi[i]) == [0, -1]:
+            full_roi[i] = True
+
+    full_roi_all = np.all(full_roi)
     
     if config["extract_candidates"]:
         """
@@ -208,19 +266,19 @@ def track(config_path):
         already extracted candidates.
         """
 
-        if config["prob_map_chunks_perp_dir"] is None:
+        if config["prob_map_chunks_perp_dir"] == "None":
             """
             Check if chunking of the probability maps needs to
             be performed, otherwise the chunk dir specified is used.
             """
 
-            dir_perp, dir_par = chunk(volume_shape=config["volume_shape"],
-                                      max_chunk_shape=config["max_chunk_shape"],
-                                      voxel_size=config["voxel_size"],
-                                      overlap=config["chunk_overlap"],
-                                      prob_map_perp_dir=config["prob_maps_perp_dir"],
-                                      prob_map_par_dir=config["prob_maps_par_dir"],
-                                      output_dir=config["chunk_output_dir"])
+            dir_perp, dir_par = chunk_pms(volume_shape=config["volume_shape"],
+                                          max_chunk_shape=config["max_chunk_shape"],
+                                          voxel_size=config["voxel_size"],
+                                          overlap=config["chunk_overlap"],
+                                          prob_map_perp_dir=config["prob_maps_perp_dir"],
+                                          prob_map_par_dir=config["prob_maps_par_dir"],
+                                          output_dir=config["chunk_output_dir"])
 
             """
             Update the config with the new output dirs for 
@@ -239,16 +297,6 @@ def track(config_path):
         Check which Chunks hold the ROI.
         """
         
-        """
-        If all rois are [0,-1] extract candidates from
-        all chunks
-        """
-        full_roi = np.array([False, False, False])
-        for i in range(3):
-            if list(roi[i]) == [0, -1]:
-                full_roi[i] = True
-
-        full_roi = np.all(full_roi)
 
         """
         Extract id, and volume information from chunks
@@ -257,7 +305,6 @@ def track(config_path):
         chunk_limits = {}
         chunk_ids = {}
         chunk_offsets = {}
-        roi_offset = []
         roi_chunks = []
 
         for f_chunk_perp, f_chunk_par in zip(chunks_perp, chunks_par):
@@ -279,25 +326,18 @@ def track(config_path):
             chunk_limit = attrs_perp[1][1]
             chunk_id = attrs_perp[0][1]
 
-            # Determine roi offset relative to volume
-            chunk_offset = [chunk_limits[j][0] for j in range(3)]
-            if not roi_offset:
-                roi_offset = chunk_offset
-                
-            for j in range(3):
-                if chunk_offset[j] < roi_offset[j]:
-                    roi_offset[j] = chunk_offset[j]
-
             chunk_limits[(f_chunk_perp, f_chunk_par)] = chunk_limit 
             chunk_ids[(f_chunk_perp, f_chunk_par)] = chunk_id
-            chunk_offsets[]
 
-            if full_roi:
+            if full_roi_all:
                 roi_chunks.append((f_chunk_perp, f_chunk_par))
             else:
                 full_ovlp = np.array([False, False, False])
                 for i in range(3):
-                    full_ovlp[i] = check_overlap(chunk_limit[i], roi[i])
+                    if list(roi[i]) == [0, -1]:
+                        full_ovlp[i] = True
+                    else:
+                        full_ovlp[i] = check_overlap(chunk_limit[i], roi[i])
 
                 if np.all(full_ovlp):
                     roi_chunks.append((f_chunk_perp, f_chunk_par))
@@ -318,38 +358,108 @@ def track(config_path):
                           voxel_size=config["voxel_size"],
                           overwrite=config["overwrite_candidates"])
 
+        # Update roi volume size:
+        roi_volume_size = [cl[1] - cl[0] for cl in chunk_limits.values()[0]] *\
+                       config["voxel_size"] * len(roi_chunks)
+
+        """
+        Determine roi offset relative to initial chunk volume.
+        We set the offset as the minimum chunk offset in each 
+        dimension.
+        """
+        roi_offset = []
+        for chunk, chunk_limit in chunk_limits.iteritems():
+            chunk_offset = [chunk_limit[j][0] for j in range(3)]
+
+            if not roi_offset:
+                roi_offset = chunk_offset
+                
+            for j in range(3):
+                if chunk_offset[j] < roi_offset[j]:
+                    roi_offset[j] = chunk_offset[j]
+
+    else:
+        if np.any(full_roi):
+            raise ValueError("Provide explicit ROI if no candidate extraction is performed")
+        
+        roi_offset = np.array([r[0] for r in roi]) * config["voxel_size"]
+        roi_volume_size = np.array([r[1] - r[0] for r in roi]) * config["voxel_size"]
+        
+
     """
     Solve the ROI and write to specified database. The result
     is written out depending on the options in the Output section
     of the config file. The collection defaults to /microtubules/.
     """
 
-    # Update roi volume size:
-    roi_volume_size = [cl[1] - cl[0] for cl in chunk_limits.values()[0]] *\
-                        config["voxel_size"] * len(roi_chunks)
+
+    """
+    Calculate roi limits
+    """
+
+    x_lim_roi = {"min": roi_offset[0],
+                 "max": roi_offset[0] + roi_volume_size[0]}
+    y_lim_roi = {"min": roi_offset[1],
+                 "max": roi_offset[1] + roi_volume_size[1]}
+    z_lim_roi = {"min": roi_offset[2],
+                 "max": roi_offset[2] + roi_volume_size[2]}
+
 
     solve_candidate_volume(name_db=config["db_name"],
                            collection="candidates",
-                           core_size=config["core_size"],
-                           context_size=config["context_size"],
+                           distance_threshold=config["distance_threshold"],
+                           cc_min_vertices=config["cc_min_vertices"],
+                           start_edge_prior=config["start_edge_prior"],
+                           selection_cost=config["selection_cost"],
+                           distance_factor=config["distance_factor"],
+                           orientation_factor=config["orientation_factor"],
+                           comb_angle_factor=config["comb_angle_factor"],
+                           time_limit=config["time_limit_per_cc"],
+                           hcs=config["get_hcs"],
+                           core_size=config["core_size"] * config["voxel_size"],
+                           context_size=config["context_size"] * config["voxel_size"],
                            volume_size=roi_volume_size,
-                           min_core_overlap=config["min_core_overlap"],
+                           min_core_overlap=config["min_core_overlap"] * config["voxel_size"],
+                           voxel_size=config["voxel_size"],
+                           output_dir=config["output_dir"],
+                           save_candidates=config["save_candidates"],
+                           save_cores=config["save_cores"],
+                           gt=config["gt"],
+                           nml=config["nml"],
+                           x_lim_out=x_lim_roi,
+                           y_lim_out=y_lim_roi,
+                           z_lim_out=z_lim_roi,
                            copy_candidates=True,
                            copy_target="microtubules",
-                           offset=np.array())
+                           offset=np.array(roi_offset)) 
 
-    # Clean up all remaining degree 0 vertices found in the context area:
+    """
+    Clean up all remaining degree 0 vertices in context area inside
+    the solved ROI.
+    """
     clean_up(name_db=config["db_name"],
              collection="microtubules",
-             x_lim=)  
-         
+             x_lim=x_lim_roi,
+             y_lim=y_lim_roi,
+             z_lim=z_lim_roi)
+
+    if config['save_roi']:
+        vertices, edges = solver.get_subgraph(name_db,
+                                              collection,
+                                              x_lim=x_lim_roi,
+                                              y_lim=y_lim_roi,
+                                              z_lim=z_lim_roi)
+
+        g1, index_map = solver.subgraph_to_g1(vertices, edges, set_partner=False)
         
+        if config["gt"]:
+            g1.save(os.path.join(config["output_dir"], "roi.gt"))
         
-        print chunk_ids
-        print chunk_limits
-        print roi_chunks 
-            
+        if config["nml"]:
+            g1_to_nml(g1, 
+                      os.path.join(config["output_dir"], "roi.nml"), 
+                      knossos=True, 
+                      voxel_size=config["voxel_size"])
         
- 
 if __name__ == "__main__":
     track("../config.ini")
