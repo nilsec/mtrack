f4d45d37e18d0fa1a6c89aa8d24c70724bc22158
Nils <nils.eckstein@googlemail.com>
2018-02-09 20:24:19 +0100

    Add vanilla solver

--- mtrack/cores/__init__.py
+++ mtrack/cores/__init__.py
@@ -2,3 +2,5 @@ from .core_scheduler import CoreScheduler
 from .core import Core
 from .core_builder import CoreBuilder
 from .core_solver import CoreSolver
+from .exception_handler import ExceptionWrapper
+from .vanilla_solver import VanillaSolver
