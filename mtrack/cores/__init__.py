bfa3b1e861f569b7f9c1a3a13ae46da1de612591
Nils <nils.eckstein@googlemail.com>
2018-05-09 17:40:28 +0200

    Rename

--- mtrack/cores/__init__.py
+++ mtrack/cores/__init__.py
@@ -4,3 +4,4 @@ from .core_builder import CoreBuilder
 from .core_solver import CoreSolver
 from .exception_handler import ExceptionWrapper
 from .vanilla_solver import VanillaSolver
+from .gen_context import get_cfs, get_core_cfs
