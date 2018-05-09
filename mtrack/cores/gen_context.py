d40077fd44cc7e69995440e6ba14b225bc59f35b
Nils <nils.eckstein@googlemail.com>
2018-05-09 14:51:26 +0200

    Clean up

---
+++ mtrack/cores/gen_context.py
@@ -0,0 +1,68 @@
+import numpy as np
+from fractions import gcd 
+import time
+
+
+def lcm(denominators):
+    return reduce(lambda a,b: a*b // gcd(a,b), denominators)
+
+
+def get_cfs(cube, nbsizes, pad=True, debug=True):
+    """
+    Generates conflict free sets for a 3d cube with arbitrary
+    shape and neighbourhood size. Large inbalance of dimensions and/or 
+    large neighbourhood sizes lead to more lists with lesser entries each.
+    Not each list is guaranteed to have the same amount of entries. This is 
+    only true if each axis has the same dimension and this dimension is divisible
+    by the neighborhood size.
+    """
+
+    dim = 3
+    nbsize = max(nbsizes)
+    n_lists = nbsize**dim
+    cf_lists = []
+
+    cube_shape = np.shape(cube)
+    cube_size = cube_shape[0] * cube_shape[1] * cube_shape[2]
+    padded_dim = lcm([cube_shape[0], cube_shape[1], cube_shape[2], nbsize])
+
+    # Pad cube to have side lengths of lcm(cube_shape, neighborhood)
+    padded_cube = -1 * np.ones([padded_dim] * 3, dtype=int)
+    padded_cube[:cube_shape[0], :cube_shape[1], :cube_shape[2]] = cube
+    cube = padded_cube
+
+    cf_lists = []
+    for n in range(n_lists):
+        cf = []
+        # Iterate over all offsets:
+        p0 = np.array([n%nbsize, (n/nbsize)%nbsize, n/(nbsize**2)%nbsize])
+        cf = [p0 + np.array([nbsize * i, nbsize * j, nbsize * k]) for i in range(padded_dim/nbsize)\
+                                                                  for j in range(padded_dim/nbsize)\
+                                                                  for k in range(padded_dim/nbsize)]
+
+        tmp = [cube[m[0], m[1], m[2]] for m in cf if cube[m[0],m[1],m[2]] != -1]
+        if tmp:
+            cf_lists.append(tmp)
+
+    if debug:
+        check_conflicts(cf_lists, cube_size)
+
+    return cf_lists
+
+
+def check_conflicts(cf_lists, max_id):
+    all_entries = np.array(sorted([i for cf in cf_lists for i in cf]))
+    expected_entries = np.arange(max_id)
+    assert(np.all(all_entries == expected_entries))
+
+    n_lists = len(cf_lists)
+    for i in range(n_lists):
+        for j in range(n_lists):
+            if i>j:
+                assert(not (set(cf_lists[i]) & set(cf_lists[j])))
+
+
+if __name__ == "__main__":
+    cube = np.arange(6*6*6).reshape(6,6,6)
+    cf_lists = get_cfs(cube, [2,2,12])
+    check_conflicts(cf_lists, 6*6*6)
