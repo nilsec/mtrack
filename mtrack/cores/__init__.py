a6eb8deb23cd04ab7eab8ee154af4d51b05efc08
Nils <nils.eckstein@googlemail.com>
2018-06-25 13:50:37 +0200

    Get rid of obsolete import

--- mtrack/cores/__init__.py
+++ mtrack/cores/__init__.py
@@ -2,6 +2,5 @@ from .core_scheduler import CoreScheduler
 from .core import Core
 from .core_builder import CoreBuilder
 from .core_solver import CoreSolver
-from .exception_handler import ExceptionWrapper
 from .vanilla_solver import VanillaSolver
 from .gen_context import get_cfs, get_core_cfs
