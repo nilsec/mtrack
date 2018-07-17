14e9d5b5eb16e71a7b406091fcf25805de0b06d3
Nils <nils.eckstein@googlemail.com>
2018-07-17 18:39:27 +0200

    Add DB layer

--- mtrack/cores/__init__.py
+++ mtrack/cores/__init__.py
@@ -4,3 +4,4 @@ from .core_builder import CoreBuilder
 from .core_solver import CoreSolver
 from .vanilla_solver import VanillaSolver
 from .gen_context import get_cfs, get_core_cfs
+from .db import DB
