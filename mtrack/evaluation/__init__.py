58dbcebe8fa350e7022414e7046157a23947ae8b
Nils Eckstein <ecksteinn@ecksteinn-ws1.int.janelia.org>
2018-11-08 07:51:42 -0500

    Update init

--- mtrack/evaluation/__init__.py
+++ mtrack/evaluation/__init__.py
@@ -2,3 +2,4 @@ from .dda3 import DDA3
 from .evaluate import evaluate_matching_graph, build_matching_graph
 from .voxel_skeleton import VoxelSkeleton
 from .matching_graph import MatchingGraph
+from .parser import EvaluationParser
